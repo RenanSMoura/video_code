99f153329a13b5126f80890f1b5ef153447f98b1
Renan Moura <renan.moura@ambevtech.com.br>
2022-02-21 20:38:18 -0300

    enf class 3.4

--- app.py
+++ app.py
@@ -2,10 +2,44 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# POST /store data: {name:}
 
-@app.route("/")
-def home():
-    return "Hellow World"
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 13.93
+            }
+        ]
+    }
+]
+
+
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
+
+
+@app.route("/store")
+def get_store_list():
+    pass
+
+
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_store_item():
+    pass
+
+
+@app.route("/store/<string:name>/item")
+def get_store_item(name):
+    pass
 
 
 app.run(port=5000)
