fdf161286549f7e31de87aa656d09ec27b56377e
Renan Moura <renan.moura@ambevtech.com.br>
2022-02-23 19:23:52 -0300

    finish sectgion 4

--- section_4/4_2/code/app.py
+++ section_4/4_2/code/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask
 from flask_jwt import JWT, jwt_required
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 from security import authenticate, identity
 
@@ -20,13 +20,42 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
+
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': f"An item with {name} already exists."}, 400
 
-        request_data = request.get_json()
-        item = {'name': name, 'price': request_data['price']}
+        data = self._check_and_get_parser()
+
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = self._check_and_get_parser()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
+
+    def _check_and_get_parser(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'price',
+            type=float,
+            required=True,
+            help='This field cannot be left blank!')
+
+        return parser.parse_args()
 
 
 class ItemList(Resource):
